cc56878628bf108c0a2f90cccfc619897763ebe4
afranzio <maniafranzio1234@gmail.com>
2023-03-29 11:04:54 +0530

    Update contact.php

--- uncode/contact.php
+++ uncode/contact.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Services</title>
+    <title>Contact</title>
 
     <!-- Styles and Fonts -->
     <!-- Favicon -->
